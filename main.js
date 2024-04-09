31bc0af279d76a79a74d8e56fd08e05e1cfffb9f
mutua-muvevi <samstore637@gmail.com>
2024-04-09 19:12:56 +0300

    made the intersection I to only  be between C and D

--- main.js
+++ main.js
@@ -4,18 +4,37 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let angle = 0
+
 //points
-const A = { x: 200, y: 50 };
+const A = { x: 200, y: 150 };
 const B = { x: 150, y: 250 };
 const C = { x: 50, y: 100 };
 const D = { x: 250, y: 200 };
 
 let points = [A, B, C, D];
 
-let t = -0.1;
+const mouse = {
+	x: 0,
+	y: 0,
+};
+
+document.onmousemove = (event) => {
+	mouse.x = event.x;
+	mouse.y = event.y;
+}
+
 animate();
 
 function animate() {
+	const radius = 50
+
+	A.x = mouse.x + Math.cos(angle) * radius
+	A.y = mouse.y- Math.sin(angle) * radius
+	B.x = mouse.x - Math.cos(angle) * radius
+	B.y = mouse.y+ Math.sin(angle) * radius
+	angle += 0.01
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	ctx.beginPath();
@@ -32,29 +51,17 @@ function animate() {
 	drawDot(C, "C");
 	drawDot(D, "D");
 
-	//calculating the midpoint using linear interpolation
-	const M = {
-		x: lerp(A.x, B.x, t),
-		y: lerp(A.y, B.y, t),
-	};
+	const I = getIntersection(A, B, C, D);
 
-	const N = {
-		x: lerp(C.x, D.x, t),
-		y: lerp(C.y, D.y, t)
+	if(I){
+		drawDot(I, "I");
 	}
 
-	drawDot(M, "M", t<0 || t>1);
-	drawDot(N, "N", t<0 || t>1);
-
-	const I = getIntersection(A, B, C, D);
-	drawDot(I, "I");
-
-	t += 0.005;
 	requestAnimationFrame(animate);
 }
 
-function getIntersection (A, B, C, D){
-/**
+function getIntersection(A, B, C, D) {
+	/**
  * formula for getting the intersection
  * Ix = Ax + t(Bx - Ax) = Cx + u(Dx - Cx) formula for geting x coordinate of intersection I
  * Iy = Ay + t(By - Ay) = Cy + u(Dy - Cy) formula for geting y coordinate of intersection I
@@ -70,15 +77,26 @@ function getIntersection (A, B, C, D){
 
  */
 
-	const top = (D.x - C.x)*(A.y - C.y) - (D.y - C.y)*(A.x - C.x);
-	const bottom = (D.y - C.y)*(B.x - A.x) - (D.x - C.x)*(B.y - A.y);
-	const t = top / bottom;
+	const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+	const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+	const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
-	// returning the point of intersection
-	return {
-		x: lerp(A.x, B.x, t),
-		y: lerp(A.y, B.y, t)
+	if(bottom !== 0){
+		const t = tTop / bottom;
+		const u = uTop / bottom;
+		if(t>=0 && t <=1 && u >=0 && u <= 1){
+			// returning the point of intersection
+			return {
+				x: lerp(A.x, B.x, t),
+				y: lerp(A.y, B.y, t),
+				offset: t
+			};
+
+		}
 	}
+
+	return null
+
 
 }
 
